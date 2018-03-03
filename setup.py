c2899e98e0016991aa266aae14b1ab6b1d1301be
Vinyas N S <vinyas.n.s@gmail.com>
2018-03-03 17:01:11 +0530

    basic setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='silent',
+    version='0.1',
+    packages=['silent'],
+    url='',
+    license='GPLv3',
+    author='Vinyas N S',
+    author_email='vinyasns@gmail.com',
+    description='A commandline client for file.io',
+    install_requires=['pycurl']
+)
